0a0fd1baefd8cfa13dd7309a28bcd824d579db54
W3journey <web3journey@proton.me>
2023-03-22 13:18:39 +0100

    Changed Toggle Button from text to icon

--- components/ToggleDarkMode.js
+++ components/ToggleDarkMode.js
@@ -1,12 +1,23 @@
-import { Button, useColorMode } from "@chakra-ui/react";
+import { Button, useColorMode, IconButton, Flex } from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
 const ToggleDarkMode = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <header>
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === "light" ? "Dark" : "Light"}
-      </Button>
+      <Flex>
+        <IconButton
+          icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+          mt="2"
+          mr="2"
+          ml="auto"
+          size="md"
+          variant={"ghost"}
+          colorScheme="teal"
+          aria-label="Toggle Color Mode"
+          onClick={toggleColorMode}
+        />
+      </Flex>
     </header>
   );
 };
